81f7555e504d1fb1522f8768c5fad9bd4fd67604
Wellnaidy Adrian <wellnaidyadrian@Wellnaidys-MacBook-Air.local>
2024-01-28 11:50:48 +0700

    never type

--- src/app/neverType/page.tsx
+++ src/app/neverType/page.tsx
@@ -1,4 +1,12 @@
 export default function NeverType(){
+    // Used for function that never return value, more strict than void
+    
+    const generateError = (message: string, code: number): never =>{
+        throw {message, statusCode: code};
+    }
+
+    generateError('Error occured!!', 500)
+
     return (
         <>
         <h1>Never Type Page</h1>
